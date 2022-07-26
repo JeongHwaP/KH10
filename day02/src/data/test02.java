15c5077b479fd52c8460aca117d268627553f8bc
JeongHwaP <pjm2327@naver.com>
2022-07-26 11:34:02 +0900

    중국집 금액 계산

---
+++ day02/src/data/test02.java
@@ -0,0 +1,17 @@
+package data;
+//(Q) 자장면 5천원, 짬뽕 6천원일 때 자장면 2그릇 + 짬뽕 3그릇 가격?
+import java.lang.*;
+public class test02 {
+	public static void main(String[] args) {
+		System.out.println("자장면 5천원, 짬뽕 6천원일 때 자장면 2그릇 + 짬뽕 3그릇 가격?");
+		System.out.println("5000 * 2");
+		System.out.println(5000 * 2);
+		System.out.println("6000 * 3");
+		System.out.println(6000 * 3);
+		//System.out.println("5000 * 2 + 6000 * 3");
+		System.out.println(5000 * 2 + 6000 * 3);//계산을 해서 표기해줌
+		
+		//System.out.println(28000);//System.out.println(5000*2 + 6000*3); 이 코드에서 계산을 해서 표기를 해주기에 불필요
+	}
+
+}
