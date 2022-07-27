3c000eb3f089914d7c63b56f979ffe5f4d53b85e
JeongHwaP <pjm2327@naver.com>
2022-07-27 09:57:38 +0900

    bmi 과제 풀이 및 보충 추가

--- day02/src/data/Test14.java
+++ day02/src/data/Test14.java
@@ -4,18 +4,38 @@ package data;
 //키가 180cm이고, 체중이 80kg인 사람의 BMI 지수를 구하여 출력
 //(네이버에 BMI 계산기 라고 검색하면 완성된 프로그램이 나옵니다.)
 import java.lang.*;
+import java.text.DecimalFormat;
 public class Test14 {
 	public static void main(String[] args) {
-		//준비
-		float height = 180;
-		float weight = 80;
+		//준비 - 신장(cm), 체중(kg)
+		//float height = 180;
+		//float weight = 80;
+		double height = 180;
+		double weight = 80;
 		
-		//계산
-		float heightM = height / 100;
-		float BMI = weight / (heightM * heightM);
+		//계산 - BMI
+		//(공식) BMI = 체중(kg) / 키(m)²
+		//float heightM = height / 100;
+		//float BMI = weight / (heightM * heightM);
+		
+		double heightMeter = height / 100;
+		
+		//double bmi = weight / 키 / 키;
+		//double bmi = weight / (키 * 키);
+		double bmi = weight / (heightMeter * heightMeter);
 		
 		//출력
 		System.out.println("BMI결과");
-		System.out.println(BMI);
+		//System.out.println(BMI);
+		System.out.println(bmi);
+		
+		//추가 : 소수점을 2자리까지만 표시
+		//(1) 직접 구현
+		double bmi2 = (int)(bmi * 100) / 100.0;
+		System.out.println(bmi2);
+		
+		//(2) 라이브러리 사용
+		DecimalFormat fm = new DecimalFormat("0.00");
+		System.out.println(fm.format(bmi));
 	}
 }
