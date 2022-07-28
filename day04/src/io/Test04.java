12e050a8734537dd8a3ffa0ab16092f9d3bae966
JeongHwaP <pjm2327@naver.com>
2022-07-28 14:59:03 +0900

    중국집 계산기 + 입력

---
+++ day04/src/io/Test04.java
@@ -0,0 +1,30 @@
+package io;
+import java.lang.*;
+import java.util.Scanner;
+public class Test04 {
+	public static void main(String[] args) {
+		//중국집 금액 계산 프로그램
+		
+		//준비
+		
+		//사장님이 정하는 정보
+		int jja = 5000;
+		int cham = 6000;
+
+		//손님이 정하는 정보 (입력)
+		Scanner sc = new Scanner(System.in);
+		System.out.println("자장면 개수를 입력하세요.");
+		int jjaCount = sc.nextInt();
+		System.out.println("짬뽕 개수를 입력하세요.");
+		int chamCount = sc.nextInt();
+		sc.close();
+		
+		//계산
+		int jjaTotal = jja * jjaCount;
+		int chamTotal = cham * chamCount;
+		int total = jjaTotal + chamTotal;
+		
+		//출력
+		System.out.println("결제 금액 : " + total);
+	}
+}
