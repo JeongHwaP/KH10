69585856f6a37d6c82da5849d748e64e18fef2d4
JeongHwaP <pjm2327@naver.com>
2022-07-28 11:12:24 +0900

    지하철 요금 계산기

---
+++ day04/src/condition/Test09.java
@@ -0,0 +1,33 @@
+package condition;
+import java.lang.*;
+import java.util.Calendar;
+public class Test09 {
+	public static void main(String[] args) {
+		//지하철 요금은 다음과 같습니다.
+		// - 어르신(65세~) : 무료, 성인(20~64세)	: 1250원
+		// - 청소년(14~19세) : 720원, 어린이(8~13세) : 450원
+		// - 영유아(~7세) : 무료
+		// 출생년도 4자리가 주어졌을 때 지하철 요금을 계산하여 출력하는 프로그램을 구현
+		
+		//준비
+		int now = Calendar.getInstance().get(Calendar.YEAR);
+		int year = 1995;
+		
+		//계산
+		int age = now - year + 1;
+		
+		//출력
+		if(age >= 20 && age <=64) {
+			System.out.println("요금 1250원 입니다.");
+		}
+		else if(age >= 14 && age <= 19) {
+			System.out.println("요금 720원 입니다.");
+		}
+		else if(age >= 8 && age <= 13) {
+			System.out.println("요금 450원 입니다.");
+		}
+		else {
+			System.out.println("무료입니다.");
+		}
+	}
+}
---
+++ day04/src/condition/Test09_3.java
@@ -0,0 +1,38 @@
+package condition;
+import java.lang.*;
+import java.util.Calendar;
+public class Test09_3 {
+	public static void main(String[] args) {
+		//지하철 요금은 다음과 같습니다.
+		// - 어르신(65세~) : 무료, 성인(20~64세)	: 1250원
+		// - 청소년(14~19세) : 720원, 어린이(8~13세) : 450원
+		// - 영유아(~7세) : 무료
+		// 출생년도 4자리가 주어졌을 때 지하철 요금을 계산하여 출력하는 프로그램을 구현
+		
+		//준비
+		int now = Calendar.getInstance().get(Calendar.YEAR);
+		//int year = 1995;
+		int birth = 19950629;
+		int year = birth / 10000;
+		
+		//계산
+		int age = now - year + 1;
+		//int price = 0 or 1250 or 720 or 450;
+		int price;
+		if(age < 8 || age >= 65) {
+			price = 0;
+		}
+		else if(age >= 20) {
+			price = 1250;
+		}
+		else if(age >= 14) {
+			price = 720;
+		}
+		else {
+			price = 450;
+		}
+		
+		//출력
+		System.out.println("요금 : " + price + "원");
+	}
+}
