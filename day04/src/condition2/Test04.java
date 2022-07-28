91a91fa3203f12c93b65d713be1533a187a1f7e8
JeongHwaP <pjm2327@naver.com>
2022-07-28 19:06:23 +0900

    월별 일수 구하기 윤년 포함

--- day04/src/condition2/Test04.java
+++ day04/src/condition2/Test04.java
@@ -31,7 +31,7 @@ public class Test04 {
 				max = 31;
 		}
 		
-		if(year%4 == 0 || year%400 == 0 && year%100 == 0) {
+		if(year%4 == 0 || year%400 == 0 && year%100 != 0) {
 			System.out.println(max + 1);
 		}
 		else if(year%4 == 0 && year%100 ==0) {
