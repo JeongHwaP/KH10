2fdb5355632cea0b3b789b037bb419e577e3d4a4
JeongHwaP <pjm2327@naver.com>
2022-07-29 11:06:04 +0900

    반복의 범위와 반복수의 활용

---
+++ day05/src/loop/Test03.java
@@ -0,0 +1,17 @@
+package loop;
+//import java.lang.*;
+public class Test03 {
+	public static void main(String[] args) {
+		//반복수의 활용
+		
+		//ex : 1부터 100까지 출력
+		for(int i=1; i <= 100; i++) {
+			System.out.println(i);
+		}
+		
+		//ex : 100부터 1까지 출력
+		for(int i=100; i >= 1; i--) {
+			System.out.println(i);
+		}
+	}
+}
