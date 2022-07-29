8fe6921aa297f8fed9205284a962fac8f268c063
JeongHwaP <pjm2327@naver.com>
2022-07-29 10:08:16 +0900

    day05 생성

---
+++ day05/src/loop/Test01.java
@@ -0,0 +1,13 @@
+package loop;
+import java.lang.*;
+public class Test01 {
+	public static void main(String[] args) {
+		//반복(loop)
+		
+		//(ex) 화면에 Hello를 10번 출력하세요.
+		
+		x10{
+			System.out.println("Hello");
+		}
+	}
+}
