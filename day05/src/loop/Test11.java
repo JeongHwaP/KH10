5a97cd6746ae6db4281843dec617c9d594a2f6d4
JeongHwaP <pjm2327@naver.com>
2022-07-29 16:54:21 +0900

    반복문 누적 합계 구하기

---
+++ day05/src/loop/Test11.java
@@ -0,0 +1,14 @@
+package loop;
+//import java.lang.*;
+public class Test11 {
+	public static void main(String[] args) {
+		//합계 구하기
+		//ex : 1부터 10까지 더한 결과를 출력하세요.
+		
+		int total = 0;
+		for(int i = 1; i <= 10; i++) {
+			total += i;
+		}
+		System.out.println("합계 = " + total);
+	}
+}
