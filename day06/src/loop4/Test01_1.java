770d3cfa91adbdd1cb7865ff39cc4c1b1a32823a
JeongHwaP <pjm2327@naver.com>
2022-08-01 12:00:48 +0900

    do ~ while 예제

---
+++ day06/src/loop4/Test01_1.java
@@ -0,0 +1,28 @@
+package loop4;
+
+import java.util.Scanner;
+
+public class Test01_1 {
+	public static void main(String[] args) {
+		//do ~ while 반복문
+		// - 사용 빈도가 매우 낮음
+		// - 선 실행 후 검사
+		
+		//ex : 0부터 100 사이의 숫자(올바른 점수)가 입력되면 종료하는 반복문
+		Scanner sc = new Scanner(System.in);
+		
+		int score;
+		while(true) {
+			System.out.print("점수 : ");
+			score = sc.nextInt();	
+			
+			if(score >= 0 && score <= 100) {
+				break;
+			}
+		}
+		
+		System.out.println("입력된 점수는 " + score + "점 입니다.");
+		
+		sc.close();
+	}
+}
