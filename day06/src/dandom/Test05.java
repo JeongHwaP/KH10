02455e6c122f8ca4aa4ebd928ac46ef12e50a8f6
JeongHwaP <pjm2327@naver.com>
2022-08-01 17:11:03 +0900

    주사위 홀짝 카운트 풀이

---
+++ day06/src/dandom/Test05.java
@@ -0,0 +1,28 @@
+package dandom;
+
+import java.util.Random;
+
+public class Test05 {
+	public static void main(String[] args) {
+		//주사위를 100번 던졌을 때 홀수와 짝수가 각각 몇 번씩 나오는지 계산하여 출력
+		
+		Random r = new Random();
+		
+		int oddCount = 0;
+		int evenCount = 0;
+		
+		for(int i = 1; i <= 100; i ++) {
+			int dice = r.nextInt(6) + 1;
+			System.out.println(i + "번째 주사위를 던져 " + dice + "가 나왔습니다.");
+			if(dice % 2 == 1)
+				oddCount++;
+			else {
+				evenCount++;
+			}		
+		}
+		
+		System.out.println("홀수 : " + oddCount + "번");
+		System.out.println("짝수 : " + evenCount + "번");
+		
+	}
+}
