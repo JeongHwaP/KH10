0b4b9e3a3e8e7a483f99a8fd6bf92e0ddba242e9
JeongHwaP <pjm2327@naver.com>
2022-08-05 16:11:48 +0900

    국영수 등급 문제 풀이

--- day10/src/oop/getter3/Test01.java
+++ day10/src/oop/getter3/Test01.java
@@ -6,6 +6,9 @@ public class Test01 {
 		Score b = new Score("루이지", 1, 85, 85, 83);
 		Score c = new Score("쿠파", 3, 70, 60, 55);
 		
+//		a.year = 5; //비추천
+//		a.setYear(5); //추천
+		
 		a.print();
 		b.print();
 		c.print();
