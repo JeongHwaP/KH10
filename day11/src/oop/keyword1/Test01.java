9fc6e3a68a8b45e6c4eeaa0733d34cfefb5427cf
JeongHwaP <pjm2327@naver.com>
2022-08-08 10:42:28 +0900

    static 변화과정 업로드

---
+++ day11/src/oop/keyword1/Test01.java
@@ -0,0 +1,10 @@
+package oop.keyword1;
+
+public class Test01 {
+	public static void main(String[] args) {
+		System.out.println(3*5);
+		
+		Calculator c = new Calculator(3, 5);
+		System.out.println(c.getAnswer());
+	}
+}
---
+++ day11/src/oop/keyword2/Calculator.java
@@ -0,0 +1,8 @@
+package oop.keyword2;
+
+public class Calculator {
+	//메소드를 1회성 계산이 가능한 구조로 변경하여 사용
+	public int getAnswer(int left, int right) {
+		return left * right;
+	}
+}
