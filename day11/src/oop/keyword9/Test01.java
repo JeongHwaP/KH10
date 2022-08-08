c810970692ca2606bc89000c51db2e951ae371ce
JeongHwaP <pjm2327@naver.com>
2022-08-08 15:53:47 +0900

    final 문제

--- day11/src/oop/keyword9/Test01.java
+++ day11/src/oop/keyword9/Test01.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Test01 {
 	public static void main(String[] args) {
-		Member a = new Member(id, password, nickname);
+//		Member a = new Member(id, password, nickname);
 		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("아이디 : ");
 		
-		System.out.print("비밀번호 : ");
-		a.setPassword(sc.next());
-		
-		System.out.print("닉네임 : ");
-		a.setNickname(sc.next());
+//		System.out.print("비밀번호 : ");
+//		a.setPassword(sc.next());
+//		
+//		System.out.print("닉네임 : ");
+//		a.setNickname(sc.next());
 	}
 }
