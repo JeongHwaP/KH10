fa728b8fad5d3c698291173786cc4296b1e4fc8f
JeongHwaP <pjm2327@naver.com>
2022-08-16 11:53:24 +0900

    시간 생성(Date)

---
+++ day16/src/api/util/date/Test01.java
@@ -0,0 +1,16 @@
+package api.util.date;
+
+import java.util.Date;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//시간 다루기
+		Date a = new Date();
+		Date b = new Date(2022, 8, 16);
+		Date c = new Date(22, 7, 16);
+		
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+	}
+}
