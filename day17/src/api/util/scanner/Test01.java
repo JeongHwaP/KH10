398a350354244067362eed3c90ae4cdebb3eaf39
JeongHwaP <pjm2327@naver.com>
2022-08-17 10:40:22 +0900

    Scanner로 문자열 읽기

---
+++ day17/src/api/util/scanner/Test01.java
@@ -0,0 +1,25 @@
+package api.util.scanner;
+
+import java.util.Scanner;
+
+public class Test01 {
+	public static void main(String[] args) {
+		String origin = "학교종이 땡땡땡 어서모여라\n선생님이 우리를 기다리신다";
+		Scanner sc = new Scanner(origin);
+		
+//		.next() - 단어(공백 전까지)
+//		.hasNext() - 읽을 수 있는 단어가 있는지 확인하는 명령
+//		while(true) {
+//			if(sc.hasNext() == false) {
+//				break;
+//			}
+//			System.out.println(sc.next());
+//		}
+		
+		while(sc.hasNext()) {
+			System.out.println(sc.next());
+		}
+		
+		sc.close();
+	}
+}
