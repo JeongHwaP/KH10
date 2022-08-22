25b3ffb9af2259f5882af987469b4440206e9cd4
JeongHwaP <109942766+JeongHwaP@users.noreply.github.com>
2022-08-22 19:41:01 +0900

    Add files via upload

--- database/Create 09. 과제.sql
+++ database/Create 09. 과제.sql
@@ -1,6 +1,4 @@
 /*
-과제
-    
     다음 정보를 저장할 수 있도록 테이블을 생성하고 데이터 추가 구문을 만들어보세요.
     
     <테이블 요구사항>
@@ -26,3 +24,50 @@
         - 아이디 : testuser5 , 전화번호 : 010-5656-7878, 등록일 : 2021-12-31, 최종구매일 : 2022-01-31, 마일리지 : 800, 등급 : 일반
 */
 
+create sequence customer_seq; --고객 번호 시퀸스 생성
+
+create table customer( -- 테이블 생성
+customer_no number not null unique, -- 숫자로 구성, 반드시 입력, 중복 불가, 시퀸스
+customer_id varchar2(20) not null unique check(regexp_like(customer_id, '^[A-Za-z0-9]{8,20}$')), -- 반드시 입력, 중복 불가, 영문+숫자로 구성
+customer_phone char(13) not null unique check(regexp_like(customer_phone, '^010-[0-9]{4}-[0-9]{4}$')), -- 반드시 입력, 중복 불가, 010-xxxx-xxxx형식
+customer_time char(10) not null, -- YYYY-MM-DD(10), 반드시 입력
+customer_buy_time char(10) not null, -- YYYY-MM-DD(10), 반드시 입력
+customer_mileage number default 0 not null check(customer_mileage>=0), -- 0이상, 없을 수 있음
+customer_grade varchar2(12) not null check(customer_grade in ('일반', 'VIP', 'VVIP', '플래티넘')) -- 4가지 중 하나 설정
+);
+
+insert into customer(
+    customer_no, customer_id, customer_phone, customer_time, customer_buy_time, customer_mileage, customer_grade
+    ) 
+    values(
+    customer_seq.nextval, 'testuser1', '010-1212-3434', '2022-01-01', '2022-03-11', 5000, 'VIP'
+    );
+insert into customer(
+    customer_no, customer_id, customer_phone, customer_time, customer_buy_time, customer_mileage, customer_grade
+    ) 
+    values(
+    customer_seq.nextval, 'testuser2', '010-2323-4545', '2020-05-11', '2022-01-30', 7200, 'VVIP'
+    );
+insert into customer(
+    customer_no, customer_id, customer_phone, customer_time, customer_buy_time, customer_mileage, customer_grade
+    ) 
+    values(
+    customer_seq.nextval, 'testuser3', '010-3434-5656', '2021-11-26', '2021-12-25', 30000, '플래티넘'
+    );
+insert into customer(
+    customer_no, customer_id, customer_phone, customer_time, customer_buy_time, customer_mileage, customer_grade
+    ) 
+    values(
+    customer_seq.nextval, 'testuser4', '010-4545-6767', '2022-02-03', '2022-03-15', 500, '일반'
+    );
+insert into customer(
+    customer_no, customer_id, customer_phone, customer_time, customer_buy_time, customer_mileage, customer_grade
+    ) 
+    values(
+    customer_seq.nextval, 'testuser5', '010-5656-7878', '2021-12-31', '2022-01-31', 800, '일반'
+    );
+commit; -- 최종 저장
+
+select*from customer; -- 조회
+
+drop table customer; -- 테이블 삭제
