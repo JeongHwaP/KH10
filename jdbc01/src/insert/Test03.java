fc256aeff7d8f6c96f60da6da7a7c7e970490d54
JeongHwaP <pjm2327@naver.com>
2022-08-25 11:28:18 +0900

    동적 SQL 생성 문제

---
+++ jdbc01/src/insert/Test03.java
@@ -0,0 +1,48 @@
+package insert;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class Test03 {
+	public static void main(String[] args) {
+		//JDBC(Java DataBase Connectivity)
+		
+		//목표 : khacademy계정의 soccer_ranking 테이블에 데이터 insert
+		//구문 :
+		//insert into soccer_ranking(rank, nation, score)
+		//values(4, '아르헨티나', 1770.65);
+		
+		//진행순서
+		//1. 데이터베이스 로그인
+		//2. 구문 준비
+		//3. 구문 전송 및 실행
+		//4. 데이터베이스 로그아웃(생략)
+		
+		//변수
+		int rank = 6;
+		String nation = "스페인";
+		//String nation = "'||(select nation from soccer_ranking where rank=1)||'";
+		double score = 1710.13;
+		
+		//1. 로그인을 도와주는 도구를 생성(Spring 제공)
+		//- 드라이버 클래스정보, 접속정보, 계정정보, 비밀번호정보를 설정
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername("khacademy");
+		dataSource.setPassword("khacademy");
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+		
+		//2. 구문 실행 도구 생성(Spring 제공)
+		JdbcTemplate template = new JdbcTemplate(dataSource);
+		
+		//3. 구문 생성
+		String sql = "insert into FIFA(rank, nation, score) " + "values(?, ?, ?)";
+		
+		Object[] param = new Object[] {rank, nation, score};
+		
+		//3. 실행
+		template.update(sql, param);
+		
+		System.out.println("완료!");
+	}
+}
