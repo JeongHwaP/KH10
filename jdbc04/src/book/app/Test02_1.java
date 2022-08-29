66192feeb58ff31e9ada1d17e616fe65ee5e1246
JeongHwaP <pjm2327@naver.com>
2022-08-29 10:23:50 +0900

    과제 풀이

--- jdbc04/src/book/app/Test02_1.java
+++ jdbc04/src/book/app/Test02_1.java
@@ -10,24 +10,12 @@ import book.util.JdbcUtil;
 
 public class Test02_1 {
 	public static void main(String[] args) {
-		// 사용자에게 등록된 모든 도서 목록을 출력
+		//사용자에게 등록된 모든 도서 목록을 출력
+		
 		JdbcTemplate template = JdbcUtil.getTemplate();
-
 		String sql = "select * from book order by book_serial asc";
-
-		RowMapper<BookDto> mapper = (rs, idx) -> {
-			BookDto bookDto = new BookDto();
-			bookDto.setBookSerial(rs.getInt("book_serial"));
-			bookDto.setBookName(rs.getString("book_name"));
-			bookDto.setBookWriter(rs.getString("book_writer"));
-			bookDto.setBookPublisher(rs.getString("book_publisher"));
-			bookDto.setBookPrice(rs.getInt("book_price"));
-			bookDto.setBookGenre(rs.getString("book_genre"));
-			bookDto.setCreationTime(rs.getDate("creation_time"));
-			return bookDto;
-		};
-		List<BookDto> list = template.query(sql, mapper);
-		for (BookDto bookDto : list) {
+		List<BookDto> list = template.query(sql, BookDto.getMapper());
+		for(BookDto bookDto : list) {
 			System.out.println(bookDto);
 		}
 	}
--- jdbc04/src/book/dto/BookDto.java
+++ jdbc04/src/book/dto/BookDto.java
@@ -2,6 +2,8 @@ package book.dto;
 
 import java.sql.Date;
 
+import org.springframework.jdbc.core.RowMapper;
+
 public class BookDto {
 	private int bookSerial;
 	private String bookName;
@@ -10,55 +12,87 @@ public class BookDto {
 	private int bookPrice;
 	private String bookGenre;
 	private Date creationTime;
+
 	public int getBookSerial() {
 		return bookSerial;
 	}
+
 	public void setBookSerial(int bookSerial) {
 		this.bookSerial = bookSerial;
 	}
+
 	public String getBookName() {
 		return bookName;
 	}
+
 	public void setBookName(String bookName) {
 		this.bookName = bookName;
 	}
+
 	public String getBookWriter() {
 		return bookWriter;
 	}
+
 	public void setBookWriter(String bookWriter) {
 		this.bookWriter = bookWriter;
 	}
+
 	public String getBookPublisher() {
 		return bookPublisher;
 	}
+
 	public void setBookPublisher(String bookPublisher) {
 		this.bookPublisher = bookPublisher;
 	}
-	public int getBookPrice() {
-		return bookPrice;
-	}
-	public void setBookPrice(int bookPrice) {
-		this.bookPrice = bookPrice;
-	}
+
 	public String getBookGenre() {
 		return bookGenre;
 	}
+
 	public void setBookGenre(String bookGenre) {
 		this.bookGenre = bookGenre;
 	}
+
 	public Date getCreationTime() {
 		return creationTime;
 	}
+
 	public void setCreationTime(Date creationTime) {
 		this.creationTime = creationTime;
 	}
+
+	public BookDto() {
+		super();
+	}
+
 	@Override
 	public String toString() {
 		return "BookDto [bookSerial=" + bookSerial + ", bookName=" + bookName + ", bookWriter=" + bookWriter
 				+ ", bookPublisher=" + bookPublisher + ", bookPrice=" + bookPrice + ", bookGenre=" + bookGenre
 				+ ", creationTime=" + creationTime + "]";
 	}
-	public BookDto() {
-		super();
+
+	public int getBookPrice() {
+		return bookPrice;
+	}
+
+	public void setBookPrice(int bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+
+	private static RowMapper<BookDto> mapper = (rs, idx) -> {
+		BookDto bookDto = new BookDto();
+		bookDto.setBookSerial(rs.getInt("book_serial"));
+		bookDto.setBookName(rs.getString("book_name"));
+		bookDto.setBookWriter(rs.getString("book_writer"));
+		bookDto.setBookPublisher(rs.getString("book_publisher"));
+		bookDto.setBookPrice(rs.getInt("book_price"));
+		bookDto.setBookGenre(rs.getString("book_genre"));
+		bookDto.setCreationTime(rs.getDate("creation_time"));
+		return bookDto;
+	};
+
+	public static RowMapper<BookDto> getMapper() {
+		return mapper;
 	}
 }
