b16096e38f9cf8e4be39514549db2c1eb585bbd2
JeongHwaP <pjm2327@naver.com>
2022-09-14 17:33:41 +0900

    관리자 헤더 추가 및 포켓몬 현황 구현

--- springhome/src/main/java/com/kh/springhome/controller/AdminController.java
+++ springhome/src/main/java/com/kh/springhome/controller/AdminController.java
@@ -1,17 +1,31 @@
 package com.kh.springhome.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.springhome.repository.PocketMonsterDao;
 
 @Controller
 @RequestMapping("/admin")//공용주소
 public class AdminController {
+	
+	@Autowired
+	private PocketMonsterDao pocketMonsterDao;
 
 	@GetMapping("/home")
 	public String home() {
 //		return "/WEB-INF/views/admin/home.jsp";
 		return "admin/home";
+	}
+	
+	@GetMapping("/pocketmon")
+	public String pocketmon(Model model) {
+		model.addAttribute("list", pocketMonsterDao.selectCountList());
+//		return "/WEB-INF/views/admin/pocketmon.jsp";
+		return "admin/pocketmon";
 	}
 	
 }
--- springhome/src/main/java/com/kh/springhome/repository/PocketMonsterDaoImpl.java
+++ springhome/src/main/java/com/kh/springhome/repository/PocketMonsterDaoImpl.java
@@ -12,6 +12,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.kh.springhome.entity.PocketMonsterDto;
+import com.kh.springhome.vo.PocketMonsterCountVO;
 
 @Repository
 public class PocketMonsterDaoImpl implements PocketMonsterDao{
@@ -85,5 +86,23 @@ public class PocketMonsterDaoImpl implements PocketMonsterDao{
 		String sql = "delete pocket_monster where no = ?";
 		Object[] param = {no};
 		return jdbcTemplate.update(sql, param) > 0;
+	}
+	
+	//PocketMonsterCountVO에 대한 RowMapper
+	private RowMapper<PocketMonsterCountVO> countMapper = new RowMapper<PocketMonsterCountVO>() {
+		@Override
+		public PocketMonsterCountVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+			PocketMonsterCountVO vo = new PocketMonsterCountVO();
+			vo.setType(rs.getString("type"));
+			vo.setCnt(rs.getInt("cnt"));
+			return vo;
+		}
+	};
+
+	@Override
+	public List<PocketMonsterCountVO> selectCountList() {
+		String sql = "select type, count(*) cnt from pocket_monster "
+							+ "group by type order by cnt desc";
+		return jdbcTemplate.query(sql, countMapper);
 	}
 }
