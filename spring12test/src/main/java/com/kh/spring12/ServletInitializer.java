1333e30ce9000337d312322c9cf21eaaeee70c09
JeongHwaP <pjm2327@naver.com>
2022-09-15 11:35:38 +0900

    spring12 생성

--- spring12test/src/main/java/com/kh/spring12/ServletInitializer.java
+++ spring12test/src/main/java/com/kh/spring12/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.kh.spring12test;
+package com.kh.spring12;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- spring12test/src/test/java/com/kh/spring12/Spring12testApplicationTests.java
+++ spring12test/src/test/java/com/kh/spring12/Spring12testApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kh.spring12test;
+package com.kh.spring12;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
