9bd7fef0b7a9ac799ebfd42336ec322d7d036761
JeongHwaP <pjm2327@naver.com>
2022-09-15 11:14:55 +0900

    spring12 생성

---
+++ spring12test/src/main/java/com/kh/spring12test/Spring12testApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring12test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring12testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring12testApplication.class, args);
+	}
+
+}
