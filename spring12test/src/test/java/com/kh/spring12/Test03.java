1d9bdff519a031f76d231d32767b365cfda8983d
JeongHwaP <pjm2327@naver.com>
2022-09-15 13:56:28 +0900

    TDD 모듈개발

---
+++ spring12test/src/test/java/com/kh/spring12/Test03.java
@@ -0,0 +1,27 @@
+package com.kh.spring12;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kh.spring12.repository.PocketMonsterDao;
+
+@SpringBootTest
+public class Test03 {
+	
+	@Autowired
+	private PocketMonsterDao pocketMonsterDao;
+	
+	@Test
+	public void test01() {
+		//System.out.println(pocketMonsterDao);
+		assertNotNull(pocketMonsterDao);
+	}
+	
+	@Test
+	public void test02() {
+		pocketMonsterDao.insert(60, "발챙이", "물");
+	}
+}
