9cc909170899b43e5eb54da42ebf53abfbf7cd3a
JeongHwaP <pjm2327@naver.com>
2022-09-23 10:18:59 +0900

    좋아요 구현중

---
+++ springhome/src/main/java/com/kh/springhome/repository/MemberBoardLikeDao.java
@@ -0,0 +1,10 @@
+package com.kh.springhome.repository;
+
+import com.kh.springhome.entity.MemberBoardLikeDto;
+
+public interface MemberBoardLikeDao {
+	void insert(MemberBoardLikeDto dto);
+	void delete(MemberBoardLikeDto dto);
+//	MemberBoardLikeDto selectOne(MemberBoardLikeDto dto);
+	boolean check(MemberBoardLikeDto dto);
+}
