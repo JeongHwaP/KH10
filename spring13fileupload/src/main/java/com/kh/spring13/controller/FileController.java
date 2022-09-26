68dd9b8ae04ef173c9e1a9e2ecb8e0b9c89252a2
JeongHwaP <pjm2327@naver.com>
2022-09-26 11:07:04 +0900

    파일 수신 처리 및 분석

---
+++ spring13fileupload/src/main/java/com/kh/spring13/controller/FileController.java
@@ -0,0 +1,36 @@
+package com.kh.spring13.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileController {
+	
+	@GetMapping("/")
+	public String root() {
+		return "root";
+	}
+	
+	//컨트롤러에서의 파일 수신 처리
+	// -> Multipart Request를 처리한다고 부름
+	// -> Spring boot에서 내부적으로 multipartResolver를 등록
+	// -> Spring Controller에서는 MultipartFile 형태로 파일을 수신
+	// -> 기존에 사용하던 annotation 전부 지원
+	@PostMapping("/")
+	public String upload(
+			@RequestParam String uploader,
+			@RequestParam MultipartFile attachment) {
+		System.out.println("uploader = " + uploader);
+		System.out.println("attachment = " + attachment);
+		//attachment 분석
+		System.out.println("content type = " + attachment.getContentType());
+		System.out.println("name = " + attachment.getName());
+		System.out.println("original file name = " + attachment.getOriginalFilename());
+		System.out.println("size = " + attachment.getSize());
+		return "redirect:/";
+	}
+	
+}
