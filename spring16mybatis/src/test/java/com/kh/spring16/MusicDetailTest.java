156e5e192a097b40cade69d6ebcf514cf318da39
JeongHwaP <pjm2327@naver.com>
2022-11-07 17:45:32 +0900

    spring16 문제풀이

---
+++ spring16mybatis/src/test/java/com/kh/spring16/MusicDetailTest.java
@@ -0,0 +1,23 @@
+package com.kh.spring16;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kh.spring16.entity.MusicDto;
+
+@SpringBootTest
+public class MusicDetailTest {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Test
+	public void test() {
+		int musicNo = 9999;
+		MusicDto musicDto = sqlSession.selectOne("music.one", musicNo);
+		System.out.println(musicDto);
+	}
+	
+}
---
+++ spring16mybatis/src/test/java/com/kh/spring16/MusicEditTest.java
@@ -0,0 +1,36 @@
+package com.kh.spring16;
+
+import java.sql.Date;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kh.spring16.entity.MusicDto;
+
+@SpringBootTest
+public class MusicEditTest {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Test
+	public void test() {
+		MusicDto musicDto = MusicDto.builder()
+											.musicNo(24)
+											.musicTitle("바꾼제목")
+											.musicArtist("바꾼가수")
+											.musicAlbum("바꾼앨범")
+											.releaseTime(Date.valueOf("2022-11-07"))
+										.build();
+		int count = sqlSession.update("music.edit", musicDto);
+		if(count > 0) {
+			System.out.println("변경 성공");
+		}
+		else {
+			System.out.println("대상이 없습니다");
+		}
+	}
+	
+}
