623a8de663c6dacb3624625d41c1371d0f4e60a6
JeongHwaP <pjm2327@naver.com>
2022-11-08 16:26:16 +0900

    정렬 추가

--- spring17complex/src/main/java/com/kh/spring17/repository/ProductDaoImpl.java
+++ spring17complex/src/main/java/com/kh/spring17/repository/ProductDaoImpl.java
@@ -10,13 +10,14 @@ import com.kh.spring17.entity.ProductDto;
 import com.kh.spring17.vo.ProductSearchVO;
 
 @Repository
-public class ProductDaoImpl implements ProductDao {
+public class ProductDaoImpl implements ProductDao{
 	
 	@Autowired
 	private SqlSession sqlSession;
 	
 	@Override
 	public List<ProductDto> complexSearch(ProductSearchVO vo) {
-		return sqlSession.selectList("product.complex5", vo);
+		return sqlSession.selectList("product.complex7", vo);
 	}
+	
 }
--- spring17complex/src/test/java/com/kh/spring17/ProductSearchTest.java
+++ spring17complex/src/test/java/com/kh/spring17/ProductSearchTest.java
@@ -12,35 +12,46 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.kh.spring17.entity.ProductDto;
 
 @SpringBootTest
-public class ProductSerchTest {
+public class ProductSearchTest {
 
 	@Autowired
 	private SqlSession sqlSession;
 	
-	@Test
+//	@Test
 	public void test() {
-		//목록
-		//List<ProductDto> list = sqlSession.selectList("product.complex1");
+//		목록
+//		List<ProductDto> list = sqlSession.selectList("product.complex1");
 		
-		//검색
+//		검색
 //		Map<String, Object> param = new HashMap<>();
 //		param.put("type", "name");
 //		param.put("keyword", "처음");
 //		List<ProductDto> list = sqlSession.selectList("product.complex1", param);
 		
-		//복합검색
+//		복합검색
 		Map<String, Object> param = new HashMap<>();
-//		param.put("no", 1);//있을 수도 없을 수도 있다.
-//		param.put("name", "스크류");//있을 수도 없을 수도 있다.
-//		param.put("minPrice", 500);//있을 수도 없을 수도 있다.
-//		param.put("maxPrice", 3000);//있을 수도 없을 수도 있다.
-//		param.put("beginMade", "2020-01-01");//있을 수도 없을 수도 있다.
-//		param.put("endMade", "2020-06-30");//있을 수도 없을 수도 있다.
+//		param.put("no", 1);//있을 수도, 없을 수도 있다
+//		param.put("name", "스크류");//있을 수도 없을 수도
+//		param.put("minPrice", 500);//있을 수도 없을 수도
+//		param.put("maxPrice", 3000);//있을 수도 없을 수도
+//		param.put("beginMade", "2020-01-01");//있을 수도 없을 수도
+//		param.put("endMade", "2020-06-30");//있을 수도 없을 수도
 //		param.put("type", List.of("과자"));
 //		param.put("type", List.of("과자", "사탕"));
 //		param.put("type", List.of("과자", "사탕", "아이스크림"));
 		param.put("type", List.of("과자", "사탕", "아이스크림", "주류"));
 		List<ProductDto> list = sqlSession.selectList("product.complex4", param);
+		
+		for(ProductDto dto : list) {
+			System.out.println(dto);
+		}
+	}
+	
+	@Test
+	public void test2() {
+		Map<String, Object> param = new HashMap<>();
+		param.put("sort", List.of("price desc", "no asc"));
+		List<ProductDto> list = sqlSession.selectList("product.complex6", param);
 		
 		for(ProductDto dto : list) {
 			System.out.println(dto);
