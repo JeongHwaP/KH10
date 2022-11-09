7fe13b89e5a3f93aef2fd456bc3341c2e685d03f
JeongHwaP <pjm2327@naver.com>
2022-11-09 10:55:28 +0900

    Simple Mail Message 테스트

--- spring18email/src/test/java/com/kh/spring18/EmailTest.java
+++ spring18email/src/test/java/com/kh/spring18/EmailTest.java
@@ -1,5 +1,48 @@
 package com.kh.spring18;
 
+import java.util.Properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@SpringBootTest
 public class EmailTest {
 
+	@Test
+	public void test() {
+		//메일 전송을 하려면 전송 도구와 메세지가 있어야 한다,
+		//전송 도구 - JavaMailSender(Impl)
+		
+		//도구 생성 및 정보 설정
+		JavaMailSenderImpl sender = new JavaMailSenderImpl();
+		sender.setHost("smtp.gmail.com");//이메일 전송 대행업체의 HOST
+		sender.setPort(587);//이메일 전송 대행업체의 PORT
+		sender.setUsername("pjm2329");//이메일 전송 대행업체 로그인 ID
+		sender.setPassword("wxrwcqhwocxvtyhu");//이메일 전송 대행업체 로그인 PW(앱 비밀번호)
+		
+		//추가 정보 설정
+		Properties props = new Properties();//Map<String, String> 형태의 저장소
+		props.setProperty("mail.smtp.auth", "true");//인증 여부 설정(필수)
+		props.setProperty("mail.smtp.debug", "true");//디버깅 사용 여부 설정(선택)
+		props.setProperty("mail.smtp.starttls.enable", "true");//TLS 사용 여부(필수)
+		props.setProperty("mail.smtp.ssl.protocols", "TLSv1.2");//TLS 버전 설정(필수)
+		props.setProperty("mail.smtp.ssl.trust", "smtp.gmail.com");//신뢰할 수 있는 대상으로 추가(필수)
+		
+		sender.setJavaMailProperties(props);
+		
+		//메세지 작성
+		SimpleMailMessage message = new SimpleMailMessage();
+		
+		message.setTo("pjm2329@gmail.com");//수신인
+//		message.setCc("");//참조
+//		message.setBcc("");//숨은참조
+		
+		message.setSubject("테스트 이메일");//메일 제목
+		message.setText("메롱");//메일 내용
+		
+		//전송
+		sender.send(message);
+	}
 }
