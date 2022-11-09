967736f2fd5b822a769939ceeea382f734eb90b5
JeongHwaP <pjm2327@naver.com>
2022-11-09 14:55:36 +0900

    인증번호 발송 작업 테스트

---
+++ spring18email/src/test/java/com/kh/spring18/CheckCertTest.java
@@ -0,0 +1,36 @@
+package com.kh.spring18;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kh.spring18.entity.CertDto;
+
+@SpringBootTest
+public class CheckCertTest {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	String email = "pjm2329@gmail.com";
+	String serial = "938024";
+	
+	@Test
+	public void test() {
+		CertDto result = sqlSession.selectOne("cert.check", CertDto.builder()
+					.who(email)
+					.serial(serial)
+				.build());
+		System.out.println(result);
+		if(result != null) {//인증 성공
+			System.out.println("인증 성공");
+			sqlSession.delete("cert.delete", email);
+		}
+		else {
+			System.out.println("인증 실패");
+		}
+	}
+}
