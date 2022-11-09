6323866fed01c95ee9a453363578bf5df3421fc9
JeongHwaP <pjm2327@naver.com>
2022-11-09 15:56:05 +0900

    인증 메일 발송 서비스 모듈화

--- spring18email/src/test/java/com/kh/spring18/CreateCertTest2.java
+++ spring18email/src/test/java/com/kh/spring18/CreateCertTest2.java
@@ -1,9 +1,5 @@
 package com.kh.spring18;
 
-import java.text.DecimalFormat;
-import java.text.Format;
-import java.util.Random;
-
 import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +7,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
+import com.kh.spring18.component.RandomGenerator;
 import com.kh.spring18.entity.CertDto;
+import com.kh.spring18.repository.CertDao;
 
 @SpringBootTest
-public class CreateCertTest {
+public class CreateCertTest2 {
 
 	@Autowired
-	private SqlSession sqlSession;
+	private JavaMailSender javaMailSender;
 	
 	@Autowired
-	private JavaMailSender javaMailSender;
+	private RandomGenerator randomGenerator;
+	
+	@Autowired
+	private CertDao certDao;
 	
 	String email = "hwang8243@gmail.com";//사용자가 입력할 정보
 	
@@ -29,12 +30,7 @@ public class CreateCertTest {
 		//목표 : (1) 랜덤인증번호생성 -> (2) 이메일발송 -> (3) 데이터베이스 등록
 		
 		//(1)
-		Random r = new Random();
-		int number = r.nextInt(1000000);
-		System.out.println("number = " + number);
-		
-		Format f = new DecimalFormat("000000");
-		String serial = f.format(number);
+		String serial = randomGenerator.generateSerial(6);
 		
 		//(2)
 		SimpleMailMessage message = new SimpleMailMessage();
@@ -44,8 +40,9 @@ public class CreateCertTest {
 		javaMailSender.send(message);
 		
 		//(3)
-		sqlSession.delete("cert.delete", email);
+		certDao.delete(email);
 		CertDto certDto = CertDto.builder().who(email).serial(serial).build();
-		sqlSession.insert("cert.insert", certDto);
+		certDao.insert(certDto);
 	}
+	
 }
