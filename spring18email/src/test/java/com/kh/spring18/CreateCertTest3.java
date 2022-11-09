2adefbf8c9325ec925eefb37abde0e147842e860
JeongHwaP <pjm2327@naver.com>
2022-11-09 16:06:23 +0900

    인증 메일 발송 서비스 모듈화

--- spring18email/src/test/java/com/kh/spring18/CreateCertTest3.java
+++ spring18email/src/test/java/com/kh/spring18/CreateCertTest3.java
@@ -3,7 +3,9 @@ package com.kh.spring18;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
 
+import com.kh.spring18.entity.CertDto;
 import com.kh.spring18.service.EmailService;
 
 @SpringBootTest
@@ -12,10 +14,11 @@ public class CreateCertTest3 {
 	@Autowired
 	private EmailService emailService;
 	
-	String email = "pjm2329@gmail.com";//사용자가 입력할 정보
+	String email = "hwang8243@gmail.com";//사용자가 입력할 정보
 	
 	@Test
 	public void test() {
 		emailService.sendCertMail(email);
 	}
+	
 }
