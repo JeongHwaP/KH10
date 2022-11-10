bec578ac4c3d0d5e0dd0e81c42bda5bbbce08dfa
JeongHwaP <pjm2327@naver.com>
2022-11-10 13:58:16 +0900

    로그 처리 완료

---
+++ spring19logging/src/test/java/com/kh/spring19/LoggingTest3.java
@@ -0,0 +1,34 @@
+package com.kh.spring19;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+//lombok에서 클래스에 @Slf4j 애노테이션 사용 시 해당 클래스의 Logger 생성
+@Slf4j
+@SpringBootTest
+public class LoggingTest3 {
+
+	@Test
+	public void test() {
+		log.debug("hello logging");
+		
+		//logger의 좋은점 - holder 지원
+		int a = 10;
+		int b = 10;
+		System.out.println(a + "+" + b + "=" + (a+b));
+		log.debug("{} + {} = {}", a, b, a+b);
+		
+		try {
+			int c = 10 / 0;//100% error
+		}
+		catch(Exception e) {
+			//System.err.println("error");
+			//e.printStackTrace();
+			log.error("error", e);
+		}
+	}
+}
