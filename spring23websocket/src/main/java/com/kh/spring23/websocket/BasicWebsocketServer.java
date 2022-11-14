48fd3a9b135f8092fd5ceeee820d189911fefebc
JeongHwaP <pjm2327@naver.com>
2022-11-14 10:38:09 +0900

    웹소켓

--- spring23websocket/src/main/java/com/kh/spring23/websocket/BasicWebsocketServer.java
+++ spring23websocket/src/main/java/com/kh/spring23/websocket/BasicWebsocketServer.java
@@ -8,13 +8,13 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * 웹소켓 서버(WebSocket Server)
- * - HTTP 연결을 처리하는 컨트롤러와는 별개로 연결형 통신을 제공하는 서버
- * - 상속을 통해서 자격을 획득해야함
- * - (I) WebSocketHandler
- * - (C) TextWebSocketHandler, BinaryWebSocketHandler
- * - 주요 연결 및 통신 처리는 Spring에서 자동으로 수행한다.
- * - 중요한 작업(접속, 종료, 메세지 수신)은 개발자에게 처리 메소드를 제공
+ *	웹소켓 서버(WebSocket Server) 
+ *	- HTTP 연결을 처리하는 컨트롤러와는 별개로 연결형 통신을 제공하는 서버
+ *	- 상속을 통해서 자격을 획득해야함
+ *	- (I) WebSocketHandler 
+ *	- (C) TextWebSocketHandler, BinaryWebSocketHandler
+ *	- 주요 연결 및 통신 처리는 Spring에서 자동으로 수행한다
+ *	- 중요한 작업(접속, 종료, 메세지 수신)은 개발자에게 처리 메소드를 제공
  */
 @Slf4j
 @Service
@@ -27,5 +27,5 @@ public class BasicWebsocketServer extends TextWebSocketHandler {
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		log.debug("사용자 종료");
-	}
+	}	
 }
