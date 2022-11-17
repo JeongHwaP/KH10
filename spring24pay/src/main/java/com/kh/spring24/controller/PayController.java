7cbcdf0f299a9b8bb727c5875699209addaa208f
JeongHwaP <pjm2327@naver.com>
2022-11-17 10:24:57 +0900

    카카오페이 컨트롤러 구현중

--- spring24pay/src/main/java/com/kh/spring24/controller/PayController.java
+++ spring24pay/src/main/java/com/kh/spring24/controller/PayController.java
@@ -1,5 +1,8 @@
 package com.kh.spring24.controller;
 
+import java.net.URISyntaxException;
+import java.util.UUID;
+
 import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
@@ -11,13 +14,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.spring24.entity.MemberDto;
+import com.kh.spring24.service.KakaoPayService;
+import com.kh.spring24.vo.KakaoPayReadyRequestVO;
+import com.kh.spring24.vo.KakaoPayReadyResponseVO;
 
 @Controller
 public class PayController {
-
+	
 	@Autowired
 	private SqlSession sqlSession;
-	
+
 	@RequestMapping("/")
 	public String home() {
 		return "home";
@@ -44,4 +50,27 @@ public class PayController {
 		session.invalidate();
 		return "redirect:/";
 	}
+	
+	//결제 페이지 1번 - 입력창에 입력하면 결제되도록 구현
+	@GetMapping("/pay1")
+	public String pay1() {
+		return "pay1";
+	}
+	
+	@Autowired
+	private KakaoPayService kakaoPayService;
+	
+	@PostMapping("/pay1")
+	public String pay1(
+			@ModelAttribute KakaoPayReadyRequestVO vo,
+			HttpSession session) throws URISyntaxException {
+		String memberId = (String)session.getAttribute("loginId");
+		vo.setPartner_user_id(memberId);
+		vo.setPartner_order_id(UUID.randomUUID().toString());
+		
+		KakaoPayReadyResponseVO response = kakaoPayService.ready(vo);
+		return "redirect:"+response.getNext_redirect_pc_url();
+	}
+	
 }
+
