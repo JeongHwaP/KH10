317f5d89510d64c856b26b7c6a5054060a0c01f6
JeongHwaP <pjm2327@naver.com>
2022-11-18 14:34:45 +0900

    계층형 조회 구문 작성

---
+++ spring24pay/src/test/java/com/kh/spring24/PayTest08.java
@@ -0,0 +1,33 @@
+package com.kh.spring24;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kh.spring24.entity.PaymentDetailDto;
+import com.kh.spring24.entity.PaymentDto;
+import com.kh.spring24.repository.PaymentDao;
+
+@SpringBootTest
+public class PayTest08 {
+	
+	@Autowired
+	private PaymentDao paymentDao;
+	
+	@Test
+	public void test() {
+		//계층형 조회를 myBatis 도움 없이 직접 구현
+		//1. 모든 payment를 조회
+		List<PaymentDto> list = paymentDao.paymentHistory("hello1236");
+		System.out.println(list.size());
+		for(PaymentDto paymentDto : list) {
+			//2. payment에 소속된 payment_detail을 조회
+			List<PaymentDetailDto> subList = 
+				paymentDao.findPaymentDetail(paymentDto.getPaymentNo());
+			System.out.println(subList.size());
+		}
+	}
+	
+}
