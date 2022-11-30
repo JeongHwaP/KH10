7940bdcbac88887502127d8ead525bcba1b65280
JeongHwaP <pjm2327@naver.com>
2022-11-30 15:28:47 +0900

    라우팅 구현

---
+++ react/cra/home/src/components/error/NotFound.js
@@ -0,0 +1,10 @@
+
+const NotFound = props=>{
+    return(<>
+        <div>
+            <h1>존재하지 않는 페이지입니다.</h1>
+        </div>
+    </>);
+};
+
+export default NotFound;
--- react/cra/home/src/components/template/MainMenu.js
+++ react/cra/home/src/components/template/MainMenu.js
@@ -1,9 +1,17 @@
 //메인 메뉴
+import { Link } from "react-router-dom";
 
 const MainMenu = props=>{
     return(<>
         <div>
-            <h1>메뉴!</h1>
+            <h1>
+                {/* router에 대한 이동 링크는 Link 태그로 작성한다. */}
+                <Link to='/'>Home</Link>
+                &nbsp;&nbsp;
+                <Link to='/login'>Login</Link>
+                &nbsp;&nbsp;
+                <Link to='/board/list'>Board</Link>
+            </h1>
         </div>
     </>);
 };
