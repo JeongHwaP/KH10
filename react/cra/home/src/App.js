3c6ff89a83fcbe02221251c0afe4c4585fb534d8
JeongHwaP <pjm2327@naver.com>
2022-11-30 17:09:10 +0900

    로그인 페이지 구현

--- react/cra/home/src/App.js
+++ react/cra/home/src/App.js
@@ -4,10 +4,11 @@ import MainFooter from './components/template/MainFooter';
 import MainHeader from './components/template/MainHeader';
 import MainMenu from './components/template/MainMenu';
 
-import { Routes, Route } from 'react-router';
 import LoginPage from './components/page/LoginPage';
 import BoardListPage from './components/page/BoardListPage';
 import NotFound from './components/error/NotFound';
+
+import {Routes, Route} from 'react-router';
 
 const App = ()=>{
   return (
@@ -15,7 +16,7 @@ const App = ()=>{
       {/* 헤더, 메뉴, 본문, 푸터 등으로 컴포넌트를 분할해야 함 */}
       <MainHeader/>
       <MainMenu/>
-
+      
       {/* 상황에 맞게 주소에 따른 화면을 보여주도록 라우팅 처리 */}
       <Routes>
         <Route path='/' element={<MainPage/>}></Route>
@@ -23,7 +24,6 @@ const App = ()=>{
         <Route path='/board/list' element={<BoardListPage/>}></Route>
         <Route path='*' element={<NotFound/>}></Route>
       </Routes>
-
       <MainFooter/>
     </>
   );
